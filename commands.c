05c89019a8c50468847667e2cbb3a11056494ab2
Yoav Dim <yoav.dim@gmail.com>
2021-12-15 13:52:00 +0200

    with partners code

--- commands.c
+++ commands.c
@@ -7,6 +7,14 @@
 int wait_job(void* jobs, int pid, char const* line);
 int fg_pid = 0;
 
+ //my add
+ char last_cd1[MAX_LINE_SIZE];
+ char last_cd2[MAX_LINE_SIZE];
+ int flag = 0 ;
+
+ int ih =0 ;
+ char history[50][MAX_LINE_SIZE]; //
+
 //********************************************
 // function name: ExeCmd
 // Description: interperts and executes built-in commands
@@ -15,6 +23,20 @@ int fg_pid = 0;
 //**************************************************************************************
 int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 {
+
+
+	// for history command
+	if(ih<50){
+		strcpy(history[ih], cmdString);
+		ih = ih +1 ;
+	}else{
+		for(int i=0 ;i<49;i++){
+			strcpy(history[i], history[i+1]);
+		}
+		strcpy(history[49], cmdString);
+	}
+
+
 	char* cmd;
 	char* args[MAX_ARG];
 	char pwd[MAX_LINE_SIZE];
@@ -42,14 +64,69 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 	if (!strcmp(cmd, "cd") )
 	{
 
+		if(*args[1]!='-'){
+			getcwd(last_cd1, MAX_LINE_SIZE);
+			flag=1;
+
+			int ret = chdir(args[1]);
+			if(ret==0){
+				//printf(" last_cd not ----\n");
+
+			}else { // ret!=0 ... error
+				printf("smash error: > “%s” – No such file or directory",cmdString);
+                return 1;
+			}
+		}
+		if(*args[1]=='-'){
+
+			if(flag){
+				getcwd(last_cd2, MAX_LINE_SIZE);
+				int ret =chdir(last_cd1);
+				strcpy(last_cd1, last_cd2);
+
+			if(ret==0){
+				//printf(" chdir okayy");
+			}
+
+			}else{ // last_cd == NULL
+				printf("smash error: > “%s” – No such file or directory",cmdString);
+                return 1;
+			}
+
+		}
 	}
 
 	/*************************************************/
 	else if (!strcmp(cmd, "pwd"))
 	{
+		char *token;
+		char *last_token;
+		const char s[2] = "/";
+		getcwd(pwd, MAX_LINE_SIZE);
 
+		   /* get the first token */
+		token = strtok(pwd, s);
+
+		   /* walk through other tokens */
+			while( token != NULL ) {
+
+   			last_token=token;
+			token = strtok(NULL, s);
+			}
+
+		printf("%s%s\n",s, last_token);
 	}
+	/*************************************************/
+	else if (!strcmp(cmd, "history"))
+	{
 
+		//printf("ih ==%d \n",ih);
+		int n_ih = 0;
+		while(n_ih!=ih+1){
+			printf("%s\n",history[n_ih]);
+			n_ih=n_ih+1;
+		}
+	}
 	/*************************************************/
 	else if (!strcmp(cmd, "kill"))
 	{
@@ -81,9 +158,40 @@ int ExeCmd(void* jobs, char* lineSize, char* cmdString)
 			jobs_print_all(jobs);
 	}
 	/*************************************************/
+	else if (!strcmp(cmd, "diff"))
+	{
+	int dif = 0 ;
+	char str1[MAX_LINE_SIZE] ;
+	char str2[MAX_LINE_SIZE] ;
+	FILE * fd1 = fopen(args[1],"r");
+		if(fd1==NULL){
+			perror("failed in open file");
+			return 1;
+		}
+	FILE * fd2 = fopen(args[2],"r");
+		if(fd2==NULL){
+			perror("failed in open file");
+			fclose(fd1);
+            return 1;
+		}
+		while(fgets(str1, MAX_LINE_SIZE, fd1)){
+			fgets(str2, MAX_LINE_SIZE, fd2);
+			if (strcmp(str1, str2)){
+				dif =1;
+			}
+		}
+		if(fgets(str2, MAX_LINE_SIZE, fd2)){
+			dif =1;
+		}
+		printf("%d\n", dif);
+        fclose(fd1);
+        fclose(fd2);
+	}
+	/*************************************************/
 	else if (!strcmp(cmd, "showpid"))
 	{
-
+ 		int pid = getpid() ;
+		printf("smash pid is %d\n",pid);
 	}
 	/*************************************************/
 	else if (!strcmp(cmd, "fg"))
@@ -190,7 +298,7 @@ int wait_job(void* jobs, int pid, char const* line) {
 		  if(errno == EINTR)
 		  	continue;
 		fg_pid = 0;
-	    perror("Failed waiting for the command");
+	    //perror("Failed waiting for the command");
 	    return -1;
 	  } else {
 		fg_pid = 0;
